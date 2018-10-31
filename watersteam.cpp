82331ad9d118aa9c22ea61391d2d5f7271571cbe
sotyarama <37976830+sotyarama@users.noreply.github.com>
2018-10-31 08:36:25 +0700

    Adding some variables and temperature converter function

--- watersteam.cpp
+++ watersteam.cpp
@@ -1,5 +1,36 @@
 #include "watersteam.h"
 #include "ui_watersteam.h"
+#include "qmath.h"
+#include <QList>
+#include <QDebug>
+
+double t_Conversion_K_to_Any(int i, double j)
+{
+    if (i==0) {
+        j = j-273.15;
+    } else if (i==1) {
+        j = (j-273.15)*(9.00/5.00)+32;
+    } else if (i==2) {
+        j = j;
+    } else {
+        j = j*(9.00/5.00);
+    }
+    return(j);
+}
+
+double t_Conversion_Any_to_K(int i, double j)
+{
+    if (i==0) {
+        j = j+273.15;
+    } else if (i==1) {
+        j = (j-32)*(5.00/9.00)+273.15;
+    } else if (i==2) {
+        j = j;
+    } else {
+        j = j*(5.00/9.00);
+    }
+    return(j);
+}
 
 WaterSteam::WaterSteam(QWidget *parent) :
     QMainWindow(parent),
--- watersteam.h
+++ watersteam.h
@@ -15,6 +15,38 @@ public:
     explicit WaterSteam(QWidget *parent = 0);
     ~WaterSteam();
 
+    //REFERENCE
+    double t_Reference_region4 = 1.00;
+    double p_Reference_region4 = 1.00;
+
+    //INPUT
+    double t_Input;
+    double p_Input;
+
+    //OUTPUT
+    double t_Output;
+    double p_Output;
+
+    //FUNCTION
+    void reg_4_saturation_Temperature();
+    void reg_4_saturation_Pressure();
+
+    //COEFFICIENT
+    double n_region4[11] =   {
+                        0.00,
+                        1167.0521452767,
+                        -724213.16703206,
+                        -17.073846940092,
+                        12020.82470247,
+                        -3232555.0322333,
+                        14.91510861353,
+                        -4823.2657361591,
+                        405113.40542057,
+                        -0.23855557567849,
+                        650.17534844798,
+                        };
+
+
 private:
     Ui::WaterSteam *ui;
 };
